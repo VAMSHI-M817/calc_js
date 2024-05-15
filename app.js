c5e6027e412d6f5af7f29382ee96b9afb828cb8f
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-05-15 19:35:19 +0530

    commit

---
+++ app.js
@@ -0,0 +1,95 @@
+class Calculator {
+  constructor(PreviousOperandTextElement, CurrentOperandTextElement) {
+    this.PreviousOperandTextElement = PreviousOperandTextElement;
+    this.CurrentOperandTextElement = CurrentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.PreviousOperand = "";
+    this.CurrentOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {
+    if (number === "." && this.CurrentOperand.includes(".")) return;
+    this.CurrentOperand = this.CurrentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.CurrentOperand == "") return;
+    if (this.CurrentOperand !== "") {
+      this.evaluate();
+    }
+    this.operation = operation;
+    this.PreviousOperand = this.CurrentOperand;
+    this.CurrentOperand = "";
+  }
+
+  evaluate() {
+    let evaluated;
+    const prev = parseFloat(this.PreviousOperand);
+    const curr = parseFloat(this.CurrentOperand);
+    if (isNaN(prev) || isNaN(curr)) return;
+    switch (this.operation) {
+      case "+":
+        evaluated = prev + curr;
+        break;
+      case "-":
+        evaluated = prev - curr;
+        break;
+      case "*":
+        evaluated = prev * curr;
+      case "÷":
+        evaluated = prev / curr;
+    }
+    this.CurrentOperand = evaluated;
+    this.operation = undefined;
+    this.PreviousOperand = "";
+  }
+
+  updateDisplay() {
+    this.CurrentOperandTextElement.innerText = this.CurrentOperand;
+    this.PreviousOperandTextElement.innerText = this.PreviousOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButton = document.querySelectorAll("[data-operation]");
+const deleteButton = document.querySelector("[data-delete]");
+const allclearButton = document.querySelector("[data-all-clear]");
+const equalsButton = document.querySelector("[data-equal]");
+const PreviousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const CurrentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+
+const calculator = new Calculator(
+  PreviousOperandTextElement,
+  CurrentOperandTextElement
+);
+
+numberButtons.forEach((buttons) => {
+  buttons.addEventListener("click", () => {
+    calculator.appendNumber(buttons.innerText);
+    calculator.updateDisplay();
+  });
+});
+operationButton.forEach((buttons) => {
+  buttons.addEventListener("click", () => {
+    calculator.chooseOperation(buttons.innerText);
+    calculator.updateDisplay();
+  });
+});
+equalsButton.addEventListener("click", (buttons) => {
+  calculator.evaluate();
+  calculator.updateDisplay();
+});
+allclearButton.addEventListener("click", (buttons) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
